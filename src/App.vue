cf90b2fb9d8aa371e33f9b2b04b054e2de942143
adrians <adiskir@gmail.com>
2025-01-16 19:16:41 +0200

    kval with button

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import HelloWorld from './components/def/HelloWorld.vue'
+import TheWelcome from './components/def/TheWelcome.vue'
+import TextButton from "@/components/TextButton.vue";
+</script>
+
+<template>
+  <header>
+<!--    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />-->
+
+<!--    <div class="wrapper">-->
+<!--      <HelloWorld msg="You did it!" />-->
+<!--    </div>-->
+  </header>
+
+  <main>
+<!--    <TheWelcome />-->
+    <TextButton text="button" :action="() => {console.log('asd')}"/>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/TextButton.vue
@@ -0,0 +1,82 @@
+<template>
+<div @click="action" class="wrapper cool-border">
+  <p class="text" ><strong>{{ text }}</strong></p>
+</div>
+</template>
+
+<script>
+export default {
+  props: ['text', 'action']
+}
+</script>
+
+<style>
+:root {
+  --outer-border: 16px;
+  --inner-border: 12px;
+}
+
+.wrapper {
+  display: flex;
+  position: relative;
+  align-items: center;
+  justify-content: center;
+
+  cursor: pointer;
+
+  width: 200px;
+  height: 70px;
+  background: rgba(2,2,2, 0.5);
+  border-radius: var(--outer-border);
+}
+
+.text {
+  font-family: Ebrima;
+  font-size: 2em;
+  z-index: 2;
+  user-select: none;
+}
+
+.wrapper::before {
+  position: absolute;
+  content: '';
+  width: calc(100% - 10px);
+  height: calc(100% - 10px);
+  background: rgba(70,70,70, 1);
+  border-radius: var(--inner-border);
+  z-index: 1;
+  box-shadow: inset 1px 1px 2px 1px rgba(0, 0, 0, .3);
+}
+
+.wrapper::after {
+  position: absolute;
+  content: '';
+  width: 100%;
+  height: 100%;
+  z-index: 0;
+  border-radius: var(--outer-border);
+
+  opacity: 0;
+  background-image: conic-gradient(from var(--angle),red, yellow, green, red 360deg);
+  animation: 3s spin linear infinite;
+  transition: .4s;
+}
+
+.wrapper:hover::after{
+  opacity: 1;
+}
+
+@property --angle {
+  syntax: "<angle>";
+  initial-value: 0deg;
+  inherits: false;
+}
+@keyframes spin {
+  from {
+    --angle: 0deg;
+  }
+  to {
+    --angle: 360deg;
+  }
+}
+</style>
